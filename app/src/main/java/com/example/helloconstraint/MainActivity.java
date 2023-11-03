3b42821b0b11abc88f8bafcb921d5cd91c2cb1e8
Xavier <91749310+arseniox@users.noreply.github.com>
2023-11-03 20:50:32 +0100

    Added new constraints to constraint layout

--- app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.helloconstraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
